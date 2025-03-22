423cfd5999af41fa7444c97e53ffff4c36b6c0c4
t rahul prabhu <167653990+trahulprabhu38@users.noreply.github.com>
2025-03-22 10:20:53 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id.split("node_modules/")[1].split("/")[0]; // Creates separate vendor chunks
-          }
-        }
-      }
-    }
-  }
-});
+  plugins: [react()],
+})
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks(id) {
+//           if (id.includes("node_modules")) {
+//             return id.split("node_modules/")[1].split("/")[0]; // Creates separate vendor chunks
+//           }
+//         }
+//       }
+//     }
+//   }
+// });
